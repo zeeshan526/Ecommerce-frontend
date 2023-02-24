82a361411792610d2f7449119d3452c764e26e7b
zeeshan526 <55299089+zeeshan526@users.noreply.github.com>
2023-02-24 18:50:47 +0500

    banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Banner/Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+
+  
+    <div className="banner-Container">
+    <div className="banner-top">
+
+    <div className="banner-wrap">
+        <div className="banner-wrap1">
+           <div className="language">Language</div>
+           <div className="currency">Currency</div> 
+           <div className="call-us">Call Us  +92 0000000000</div>  
+        </div>
+           <div className="banner-offer">Annoucements and Offers</div>
+        </div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default Banner;
