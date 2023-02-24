65103117edd3806ec40345e8573d5e1ddd088c46
zeeshan526 <55299089+zeeshan526@users.noreply.github.com>
2023-02-24 20:31:15 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Banner from './components/Banner/Banner';
+import Header from './components/Header/Header';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import '../Header/Header.css'
+import logo from '../../images/logo.png'
 function Header() {
   return (
-    <div>Header</div>
+    <div className='head-container'>
+        <div className='head-wraper'>
+        <div className='head-wrapper-child'>
+        <div className='logo'><img src={logo} alt='logo'/></div>
+            <div className='head-list'> 
+                <ul >
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Products</a></li>
+                    <li><a href='#'>Contact</a></li>
+                    <li><a href='#'>About</a></li>
+                </ul>
+            </div>
+        </div>
+            
+            <div className='cart'><a href='#'>Cart</a></div>
+
+        </div>
+    </div>
   )
 }
 
